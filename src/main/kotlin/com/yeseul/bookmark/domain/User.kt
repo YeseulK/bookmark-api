c766527f6b0baddb8752a0cb8f91ec43c7626f34
Yeseul Kim <yeseullk@gmail.com>
2022-05-06 01:05:51 +0900

    fix:

--- src/main/kotlin/com/yeseul/bookmark/domain/User.kt
+++ src/main/kotlin/com/yeseul/bookmark/domain/User.kt
@@ -3,7 +3,6 @@ package com.yeseul.bookmark.domain
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.Id
-import javax.persistence.OneToMany
 
 @Entity
 data class User(
@@ -11,8 +10,5 @@ data class User(
     @GeneratedValue
     var id: Long? = null,
 
-    var name: String,
-
-    @OneToMany
-    var folder: List<Folder>
+    var name: String
 )
