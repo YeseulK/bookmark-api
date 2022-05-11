13c7489e1a0bc2156e6ae2b142ae7e67f0d35d35
Yeseul Kim <yeseullk@gmail.com>
2022-05-12 02:23:20 +0900

    refactor: request dto, response dto 분리

--- src/main/kotlin/com/yeseul/bookmark/domain/Folder.kt
+++ src/main/kotlin/com/yeseul/bookmark/domain/Folder.kt
@@ -1,11 +1,9 @@
 package com.yeseul.bookmark.domain
 
-import com.fasterxml.jackson.annotation.JsonInclude
 import com.yeseul.bookmark.utils.BaseTime
 import javax.persistence.*
 
 @Entity
-@JsonInclude(JsonInclude.Include.NON_NULL)
 data class Folder(
     @Id
     @GeneratedValue
